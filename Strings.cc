e5e914b4e73b9ed78e560b869f5f1e28a596b24d
Phil Wilshire <sysdcs@gmail.com>
2014-03-06 08:26:09 -0500

    Strings Test

---
+++ Strings.cc
@@ -0,0 +1,93 @@
+// string::find_first_of
+#include <iostream>       // std::cout
+#include <string>         // std::string
+#include <cstddef>        // std::size_t
+using namespace std;
+
+// returns 0 for just a name ,1 for lib 2 for cmd
+int DecodeName(string &sname, string &slib, string &sact, string &satr, const string& sin)
+{
+  string delims ="!@?";
+  string samp = sin;
+  size_t found = samp.find_first_of(delims);
+  char skey;
+  int rc = 0;
+  if (found==string::npos)
+    {
+      sname=samp;
+      return rc;
+    }
+  sname=samp.substr(0,found);
+  bool done = false;
+  while((delims.size() > 0) && ! done)
+    {
+      rc++;
+      skey=samp[found];
+      delims.erase(delims.find(skey),1);
+      samp.erase(0,found);
+      found=samp.find_first_of(delims);
+      cout << "rc ("<<rc<<") samp now ["<<samp<<"] delims ["<<delims<<"] key["<<skey<<"]\n";
+      if(skey == '@')
+	{
+	  slib=samp.substr(0,found);
+	}
+      if(skey == '!')
+	{
+	  sact=samp.substr(0,found);
+	}
+      if(skey == '?')
+	{
+	  satr=samp.substr(0,found);
+	}
+      done =(found==string::npos);
+    }
+  return rc;
+}
+
+void TestDecodeName(const string &sin)
+{
+  string sname;
+  string slib;
+  string sact;
+  string satr;
+  
+  cout << "sin  ["<<sin<<"] ======\n";
+  int rc = DecodeName(sname, slib, sact, satr, sin);
+  cout << "=========>";
+  cout << "rc->("<<rc<<") ";
+  cout << "sname->["<<sname<<"] ";
+  cout << "slib->["<<slib<<"] ";
+  cout << "sact->["<<sact<<"] ";
+  cout << "satr->["<<satr<<"]\n\n";
+  return;
+}
+
+int main ()
+{
+ 
+ string str ("Please, replace the vowels in this sentence by asterisks.");
+  size_t found = str.find_first_of("aeiou");
+  while (found!=string::npos)
+  {
+    str[found]='*';
+    found=str.find_first_of("aeiou",found+1);
+  }
+
+  //  cout << str << '\n';
+  TestDecodeName("foo");
+  TestDecodeName("foo@gpios");
+  TestDecodeName("foo@gpios?atrs=1&atrs=2");
+  TestDecodeName("foo@gpios?atrs=1&atrs=2!list");
+  TestDecodeName("foo@gpios!list");
+  TestDecodeName("foo@gpios!list?atrs=1&atrs=2");
+  TestDecodeName("foo!list");
+  TestDecodeName("foo!list@gpios");
+  TestDecodeName("foo!list@gpios?atrs=1&atrs=2");
+  TestDecodeName("foo!list?atrs=1&atrs=2@gpios");
+  TestDecodeName("foo?atrs=1&atrs=2");
+  TestDecodeName("foo?atrs=1&atrs=2@gpios");
+  TestDecodeName("foo?atrs=1&atrs=2@gpios!list");
+  TestDecodeName("foo?atrs=1&atrs=2!list@gpios");
+
+  return 0;
+}
